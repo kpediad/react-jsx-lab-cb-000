892648f396af35680ffc59b6566873ca0b5325b5
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-14 10:21:05 +0000

    Automatically backed up by Learn

---
+++ src/components/Webpage.js
@@ -0,0 +1,11 @@
+export class Webpage extends Component {
+  render() {
+    return (
+      <main>
+        <title>{"The world's coolest webpage"}</title>
+        <FillerText />
+        <FillerText />
+      </main>
+    )
+  }
+}
