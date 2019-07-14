0c55a8006b4bb8974c9c927dc4bb9b080f249a73
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-14 10:24:27 +0000

    Automatically backed up by Learn

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,11 @@
+export class RegistrationForm extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" />
+        <input type="password" />
+        <button type="submit" />
+      </form>
+    );
+  }
+}
